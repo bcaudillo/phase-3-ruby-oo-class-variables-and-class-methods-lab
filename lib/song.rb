770226ebedc7b7656f052acdd8225461f9420c5a
bcaudillo <btcaudillo@gmail.com>
2022-12-17 18:37:52 -0700

    commit tree d427572335a861b6e74610cc5670e4a75c22dc18

---
+++ lib/song.rb
@@ -0,0 +1,46 @@
+
+
+
+class Song
+    attr_accessor :name, :artist, :genre
+    @@count = 0
+    @@genres = []
+    @@artists = []
+    @@tally = 0
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres << genre
+        @@artists << artist
+    end
+
+    def self.count
+        @@count
+    end
+    def self.artists
+        @@artists = @@artists.uniq
+        @@artists
+    end
+    
+    def self.tally
+        @@tally
+    end
+
+    def self.genres
+        @@genres = @@genres.uniq
+        @@genres
+    end
+
+    def self.genre_count
+       @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+
+
+end
